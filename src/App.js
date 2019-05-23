2977b0a19369006d4cdc9164c3a762d69fb066d3
Tiffany Raynaud <raynatif@gmail.com>
2019-05-23 11:53:27 +0200

    board & cards feedback init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+    
+import React, { Component } from 'react'
+import './App.css'
+import GuessCount from './GuessCount';
+import Card from './Card';
+
+class App extends Component {
+  handleCardClick(card){
+    console.log(card, 'clicked');
+  }
+  
+  render() {
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😄" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="💖" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+      </div>
+    )
+  }
+}
+
+export default App
